9f47c6898c892a3de17deb457517622bea062b0f
Spencer Florence <spencer@florence.io>
2017-12-20 12:47:24 -0500

    documenting things

--- main.rkt
+++ main.rkt
@@ -1,25 +1,53 @@
 #lang racket
 (provide
+ ;; data
+ signal?
+ pure-signal?
+ value-signal?
+ reactor?
+ process?
+ reactor-done?
+ ;; running
  start&
  run&
+ react&
+ ;; process creation
  process&
  define-process&
+ ;; forms
  present&
  signal&
  par&
  emit&
  pause&
- react&
  await&
- reactor-done?
  last&)
 (require (for-syntax syntax/parse racket/stxparam-exptime racket/syntax) racket/hash racket/stxparam)
 (module+ test (require rackunit))
 
 (define current-reactor (make-parameter #f))
-(struct process (thunk) #:constructor-name make-process #:authentic)
+
+;; A Reactor is a
+;;  (reactor RThread (Listof Thread) (hasheqof Signal Blocked) (listof RThread) (Listof Signal))
 (struct reactor (os active blocked paused signals) #:mutable #:authentic)
+;; `os` is the continuation for the OS loop
+;; `active` are a list of runnable threads
+;; `blocked` maps signals to blocked thread pairs
+;; `paused` is the list of threads to activate next instant
+;; `signals` is a list of all signals in the program, that have been emitted
+;;   they may be reset inbetween instants
+
+;; a RThread is a (-> Any), and is the continuation of a thread
+
+;; a process is a (make-process RThread)
+(struct process (thunk) #:constructor-name make-process #:authentic)
+;; `thunk` is the initial thread for this process
+;; processes simply box unrun threads
+
+;; a blocked is a (make-blocked RThread RThread)
 (struct blocked (present absent) #:constructor-name make-blocked)
+;; a blocked represents a thread awaiting a signals value.
+;; it will run `present` if the signal is present or `absent` if it is now.
 
 ;; a signal is a one of
 ;; (make-value-signal boolean A (listof A) (A A -> A))
@@ -90,7 +118,6 @@
          e ...)]
     [(signal& ([S:id default:expr #:gather gather:expr] ...) e ...)
      #'(let ([S (make-value-signal #f default empty gather)] ...)
-         (register-signal! S) ...
          e ...)]))
 (define-syntax par&
   (syntax-parser
@@ -149,20 +176,28 @@
 
 (define reactive-tag  (make-continuation-prompt-tag 'react))
 
+;; Process -> Reactor
 (define (start& proc)
   (make-reactor (process-thunk proc)))
 
+;; Reactor -> Any
+;; run a reaction on this reactor
 (define (react& grp)
   (call-with-continuation-prompt
    (lambda () (os-loop! grp))
    reactive-tag))
 
+;; (-> Any) -> Reactor
+;; make a reactor containing only the given thread, which is active
 (define (make-reactor proc)
   (reactor void (list proc) (make-hasheq) empty empty))
 
+;; Reactor -> Any
+;; main scheduler loop. Should be called with a `reactive-tag`
 (define (os-loop! g)
-  (match g
-    [(reactor os (list) blocked paused signals)
+  (match-define (reactor os active blocked paused signals) g)
+  (cond
+    [(reactor-done? g)
      (define active
        (for*/list ([(_ procs) (in-hash blocked)]
                    [p (in-list procs)])
@@ -171,9 +206,10 @@
      (set-reactor-active! g (append active paused))
      (set-reactor-blocked! g (make-hasheq))
      (set-reactor-paused! g empty)
+     (set-reactor-signals! g empty)
      (for ([S signals])
        (reset-signal! S))]
-    [(reactor os active blocked paused signals)
+    [else
      (define next (first active))
      (set-reactor-active! g (rest active))
      (call/cc
@@ -184,6 +220,8 @@
       reactive-tag)
      (os-loop! g)]))
 
+;; Signal -> Void
+;; cleanup signal for next instant
 (define (reset-signal! S)
   (when (and (value-signal? S) (signal-status S))
     (define c (value-signal-collection S))
@@ -192,22 +230,33 @@
      (foldl (value-signal-gather S) (first c) (rest c)))
     (set-value-signal-collection! S empty))
   (set-signal-status! S #f))
-  
 
+;; -> Any
+;; switch back to the scheduler
 (define (switch!)
   ((reactor-os (current-reactor))))
 
-(define (register-signal! S)
+;; Signal -> Void
+;; registers a signal emission with the OS for cleanup after the instant.
+;; also unblocks any processes waiting on S
+(define (register-signal-emission! S)
   (set-reactor-signals!
    (current-reactor)
-   (cons S (reactor-signals (current-reactor)))))
+   (cons S (reactor-signals (current-reactor))))
+  (unblock! S))
 
+;; Reactor -> Boolean
+;; does this reactor have active threads?
 (define (reactor-done? g) (empty? (reactor-active g)))
 
+;; RThread -> Void
+;; register this threads as active
 (define (activate! thrd)
   (set-reactor-active!
    (current-reactor)
    (cons thrd (reactor-active (current-reactor)))))
+;; RThread -> Void
+;; register this thread for the next instant
 (define (run-next! thrd)
   (set-reactor-paused!
    (current-reactor)
@@ -215,15 +264,22 @@
 
 ;;;;;; running
 
+;; ValueSignal -> Any
 (define last& value-signal-value)
-         
+
+;; Process -> Any
 (define (runf proc) ((process-thunk proc)))
 
+;; PureSignal -> Void
+;; emit the signal
 (define (emit-pure S)
   (unless (pure-signal? S)
     (error 'emit& "Expected pure signal, given ~s" S))
   (set-signal-status! S #t)
-  (unblock! S))
+  (register-signal-emission! S))
+
+;; ValueSignal Any -> Void
+;; emit the given value on the signal
 (define (emit-value S v)
   (unless (value-signal? S)
     (error 'emit& "Expected value carrying signal, given ~s and ~s" S v))
@@ -231,8 +287,10 @@
   (set-value-signal-collection!
    S
    (cons v (value-signal-collection S)))
-  (unblock! S))
+  (register-signal-emission! S))
 
+;; Signal -> Void
+;; Unblock every thread waiting on S using its `present` continuation
 (define (unblock! S)
   (define grp (current-reactor))
   (define blocked (reactor-blocked grp))
@@ -243,6 +301,8 @@
    (append (reactor-active grp)
            (map blocked-present unblocked))))
 
+;; -> Void
+;; suspend the current thread until the next instant
 (define (pausef)
   (call/cc
    (lambda (k)
@@ -251,6 +311,8 @@
      (switch!))
    reactive-tag))
 
+;; Signal (-> Any) (-> Any) -> Any
+;; dispatch on signal status, or block if not ready yet
 (define (presentf S p q)
   (cond
     [(signal-status S) (p)]
@@ -266,9 +328,14 @@
         (switch!))
       reactive-tag)]))
 
+;; Signal -> Void
+;; block until the signal is present (including in this instant)
 (define-process& (await-immediate S)
   (present& S (void) (run& (await-immediate S))))
 
+;; ValueSignal (Any -> Any) -> Any
+;; Await a value for the signal S, and give it to `f`
+;; when ready
 (define-process& (await-value S f)
   (present& S
             (begin
@@ -276,6 +343,8 @@
               (f (last& S)))
             (run& (await-value S f))))
 
+;; (Listof RThread) -> Any
+;; run all threads, blocking the current thread until all have completed
 (define (parf threads)
   (define counter (length threads))
   ;; TODO optimization mentioned in RML paper:
